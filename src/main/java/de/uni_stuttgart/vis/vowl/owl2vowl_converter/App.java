aa17c1ad96269f0a947fb1b881d464b1474f1c95
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2014-07-13 13:48:43 +0000

    Create basic structure with maven.

---
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl_converter/App.java
@@ -0,0 +1,13 @@
+package de.uni_stuttgart.vis.vowl.owl2vowl_converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/de/uni_stuttgart/vis/vowl/owl2vowl_converter/AppTest.java
@@ -0,0 +1,38 @@
+package de.uni_stuttgart.vis.vowl.owl2vowl_converter;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
