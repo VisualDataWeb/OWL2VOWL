2ea3a914b73d316f8313fb2926d69386620feb2e
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2014-07-15 12:37:39 +0000

    Create structure.

---
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/export/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * Dummy.java
+ *
+ */
+
+package de.uni_stuttgart.vis.vowl.owl2vowl.export;
+
+/**
+ * @author Eduard Marbach
+ * @version 1.0
+ */
+public class Dummy {
+}
---
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/pipes/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * Dummy.java
+ *
+ */
+
+package de.uni_stuttgart.vis.vowl.owl2vowl.pipes;
+
+/**
+ * @author Eduard Marbach
+ * @version 1.0
+ */
+public class Dummy {
+}
