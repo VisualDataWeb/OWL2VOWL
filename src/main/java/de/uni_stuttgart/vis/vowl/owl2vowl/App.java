37022be45aa6b9b746599aa2a154af1dccd4c4d6
linkvt <mail@linkvt.de>
2014-07-15 12:23:10 +0000

    Reformat code

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/App.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/App.java
@@ -4,10 +4,8 @@ package de.uni_stuttgart.vis.vowl.owl2vowl;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
