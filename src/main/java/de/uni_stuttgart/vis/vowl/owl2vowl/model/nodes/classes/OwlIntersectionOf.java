6f5a225d872ddf7a93fe0c0d75d3cae071f081c0
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2014-10-15 18:45:25 +0000

    Add intersection extraction.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/nodes/classes/OwlIntersectionOf.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/nodes/classes/OwlIntersectionOf.java
@@ -2,11 +2,58 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes;
 
 
 import de.uni_stuttgart.vis.vowl.owl2vowl.model.Constants;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.BaseNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OwlIntersectionOf extends BaseClass {
+	private List<BaseNode> intersections;
 
 	public OwlIntersectionOf() {
 		super();
 		setType(Constants.TYPE_INTERSECTION);
+		intersections = new ArrayList<>();
+	}
+
+	public List<BaseNode> getIntersections() {
+		return intersections;
+	}
+
+	public void addIntersection(BaseNode node) {
+		if(node == null) {
+			return;
+		}
+
+		intersections.add(node);
+	}
+
+	public void setIntersections(List<BaseNode> unions) {
+		this.intersections = unions;
+	}
+
+	public boolean equalsIntersections(List<BaseNode> classes) {
+		for (BaseNode aClass : classes) {
+			if (!intersections.contains(aClass)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public boolean equalIntersectionIris(List<String> iriList) {
+		int equalIris = 0;
+
+		for (String aIri : iriList) {
+			for(BaseNode aUnion : intersections) {
+				if(aUnion.getIri().equals(aIri)) {
+					equalIris++;
+					break;
+				}
+			}
+		}
+
+		return equalIris == iriList.size();
 	}
 }
---
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/helper/IntersectionParser.java
@@ -0,0 +1,79 @@
+/*
+ * UnionParser.java
+ *
+ */
+
+package de.uni_stuttgart.vis.vowl.owl2vowl.parser.helper;
+
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.Constants;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.BaseNode;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.OwlIntersectionOf;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ */
+public class IntersectionParser extends AxiomParser {
+	Map<String, OwlIntersectionOf> intersectionNodes = mapData.getIntersectionMap();
+
+	/**
+	 * Searches for an union class of the property. If already exists take it an return else create
+	 * a new node. If no union found return null.
+	 *
+	 * @param property  The property to search in.
+	 * @param direction True for domain else range.
+	 */
+	public OwlIntersectionOf searchIntersection(OWLProperty property, boolean direction) {
+		Set<OWLClass> classes = search(property, Constants.AXIOM_OBJ_INTERSECTION, direction);
+
+		if (classes == null) {
+			return null;
+		}
+
+		OwlIntersectionOf intersectionOf = getExistingIntersectionClass(classes);
+
+		if (intersectionOf == null) {
+			intersectionOf = createIntersectionClass(classes);
+		}
+
+		return intersectionOf;
+	}
+
+	private OwlIntersectionOf createIntersectionClass(Set<OWLClass> classesInSignature) {
+		OwlIntersectionOf intersectionOf = new OwlIntersectionOf();
+
+		for (OWLClass currentClass : classesInSignature) {
+			BaseNode theNode = findNode(currentClass.getIRI().toString());
+
+			if (theNode != null) {
+				intersectionOf.addIntersection(theNode);
+			}
+		}
+
+		intersectionNodes.put(intersectionOf.getId(), intersectionOf);
+		return intersectionOf;
+	}
+
+	private OwlIntersectionOf getExistingIntersectionClass(Set<OWLClass> classes) {
+		for (Map.Entry<String, OwlIntersectionOf> entry : intersectionNodes.entrySet()) {
+			OwlIntersectionOf currentClass = entry.getValue();
+			List<String> test = new ArrayList<>();
+
+			for (OWLClass curIri : classes) {
+				test.add(curIri.getIRI().toString());
+			}
+
+			if (currentClass.equalIntersectionIris(test)) {
+				return currentClass;
+			}
+		}
+
+		return null;
+	}
+}
