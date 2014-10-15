a8df921220ca009d2a044851561cffe2bdcc875e
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2014-10-15 18:45:04 +0000

    Add new constants.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/Constants.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/Constants.java
@@ -85,5 +85,8 @@ public class Constants {
 	/* Annotations used for axioms. */
 	public static final String AXIOM_OBJ_PROP_DOMAIN = "ObjectPropertyDomain";
 	public static final String AXIOM_OBJ_PROP_RANGE = "ObjectPropertyRange";
+	public static final String AXIOM_DATA_PROP_DOMAIN = "DataPropertyDomain";
+	public static final String AXIOM_DATA_PROP_RANGE = "DataPropertyRange";
 	public static final String AXIOM_OBJ_UNION = "ObjectUnionOf";
+	public static final String AXIOM_OBJ_INTERSECTION = "ObjectIntersectionOf";
 }
