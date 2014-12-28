1fd8534aa99285e0a9a3080ecf0691b59dd41562
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2014-12-28 22:32:30 +0000

    Add property types.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/constants/Property_Types.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/constants/Property_Types.java
@@ -3,4 +3,5 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.constants;
 public class Property_Types {
 	public static final String PROP_TYPE_SUBCLASS = "rdfs:SubClassOf";
 	public static final String PROP_TYPE_DISJOINT = "owl:disjointWith";
+	public static final String PROP_TYPE_RDF = "rdf:Property";
 }
