d2d28bd901bace4bbf6e75fa5db6e770e48fb103
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2015-01-18 22:45:24 +0000

    Improve identifier cutting.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/container/Annotation.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/container/Annotation.java
@@ -9,13 +9,31 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.parser.container;
  *
  */
 public class Annotation {
+	public static final String TYPE_IRI = "iri";
+	public static final String TYPE_LABEL = "label";
 	private String identifier;
 	private String language;
 	private String value;
+	private String type;
 
 	public Annotation(String identifier, String value) {
 		this.identifier = extractIdentifier(identifier);
 		this.value = value;
+		this.type = "unset";
+	}
+
+	public Annotation(String identifier, String value, String type) {
+		this.identifier = extractIdentifier(identifier);
+		this.value = value;
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getIdentifier() {
@@ -27,7 +45,31 @@ public class Annotation {
 	}
 
 	private String extractIdentifier(String identifier) {
-		return identifier.substring(identifier.lastIndexOf("/") + 1, identifier.lastIndexOf(">"));
+		int slash = identifier.lastIndexOf("/");
+		int arrow = identifier.lastIndexOf(">");
+		int doubleDot = identifier.lastIndexOf(":");
+		int hash = identifier.lastIndexOf("#");
+
+		int lastCut = identifier.length() - 1;
+		int firstCut = 0;
+
+		if (arrow != -1) {
+			lastCut = arrow;
+		}
+
+		if (slash != -1 && firstCut < slash) {
+			firstCut = slash;
+		}
+
+		if (doubleDot != -1 && firstCut < doubleDot) {
+			firstCut = doubleDot;
+		}
+
+		if (hash != -1 && firstCut < hash) {
+			firstCut = hash;
+		}
+
+		return identifier.substring(firstCut + 1, lastCut);
 	}
 
 	public String getLanguage() {
