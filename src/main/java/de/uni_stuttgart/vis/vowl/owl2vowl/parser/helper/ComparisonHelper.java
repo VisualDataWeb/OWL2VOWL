6252068ee6398a8e602fd868c29f6e4e13f56879
linkvt <mail@linkvt.de>
2015-01-21 12:38:27 +0000

    Rewrite namespace comparison with tests

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/helper/ComparisonHelper.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/helper/ComparisonHelper.java
@@ -8,7 +8,10 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.parser.helper;
 import de.uni_stuttgart.vis.vowl.owl2vowl.parser.container.OntologyInformation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 import java.util.Set;
 
@@ -17,6 +20,7 @@ import java.util.Set;
  */
 public class ComparisonHelper {
 	private static final Logger logger = LogManager.getLogger(ComparisonHelper.class);
+
 	private static boolean hasDifferentNameSpace(String elementNamespace, OntologyInformation information) {
 		if (elementNamespace == null) {
 			logger.info("Namespace check: Element has no namespace!");
@@ -32,24 +36,37 @@ public class ComparisonHelper {
 			ontologyIri = information.getOntology().getOntologyID().getOntologyIRI().toString();
 		}
 
-		int element = elementNamespace.indexOf("#");
-		int elementSlash = elementNamespace.lastIndexOf("/");
-		int ontologyIn = ontologyIri.indexOf("#");
+		return hasDifferentNameSpace(elementNamespace, ontologyIri);
+	}
 
-		String newElement = elementNamespace;
-		String newOntology = ontologyIri;
-
-		if (element != -1) {
-			newElement = newElement.substring(0, element);
-		} else if (elementSlash != -1) {
-			newElement = newElement.substring(0, elementSlash + 1);
+	public static boolean hasDifferentNameSpace(String elementNamespace, String ontologyIri) {
+		if (elementNamespace.equals(ontologyIri)) {
+			return false;
 		}
 
-		if (ontologyIn != -1) {
-			newOntology = newOntology.substring(0, ontologyIn);
+		if (elementNamespace.equals(ontologyIri + "#")) {
+			return false;
 		}
 
-		return !(newElement.equals(newOntology));
+		if (elementNamespace.contains("#")) {
+			String elementNamespaceWithoutHashParameter = elementNamespace.split("#")[0];
+			if (elementNamespaceWithoutHashParameter.equals(ontologyIri)) {
+				return false;
+			}
+		}
+
+		if (elementNamespace.contains("/") && !elementNamespace.endsWith("/")) {
+			int lastSlashIndex = elementNamespace.lastIndexOf("/");
+			int indexAfterSlash = lastSlashIndex + 1;
+
+			String elementNamespaceWithoutLastPart = elementNamespace.substring(0, indexAfterSlash);
+
+			if (elementNamespaceWithoutLastPart.equals(ontologyIri)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	public static boolean hasDifferentNameSpace(OWLEntity entity, OntologyInformation information) {
