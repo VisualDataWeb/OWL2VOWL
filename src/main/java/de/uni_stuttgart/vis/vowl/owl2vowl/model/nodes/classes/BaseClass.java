f5e23f795a28c1f43119bfb31f5a67e7c064968f
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2015-02-06 19:36:59 +0000

    Add container for defined individuals for classes.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/nodes/classes/BaseClass.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/nodes/classes/BaseClass.java
@@ -7,7 +7,11 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes;
 
 import de.uni_stuttgart.vis.vowl.owl2vowl.constants.Node_Types;
 import de.uni_stuttgart.vis.vowl.owl2vowl.export.JsonGeneratorVisitor;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.individuals.NamedIndividual;
 import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.BaseNode;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Eduard Marbach
@@ -16,12 +20,22 @@ public class BaseClass extends BaseNode {
 
 	protected static int indexCounter = 1;
 	protected int numberOfIndividuals = 0;
+	protected Set<NamedIndividual> individuals;
 
 	public BaseClass() {
 		super();
 		setType(Node_Types.TYPE_CLASS);
 		setId("class" + indexCounter);
+		individuals = new HashSet<NamedIndividual>();
 		indexCounter++;
+	}
+
+	public Set<NamedIndividual> getIndividuals() {
+		return individuals;
+	}
+
+	public void setIndividuals(Set<NamedIndividual> individuals) {
+		this.individuals = individuals;
 	}
 
 	public int getNumberOfIndividuals() {
