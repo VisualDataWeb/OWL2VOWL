8f97b8456822d70b5e33ab0ff3e7205f94b3caba
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2015-02-06 19:36:47 +0000

    Add method for processing a collection of annotaitons.,

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/helper/AnnotationParser.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/helper/AnnotationParser.java
@@ -8,9 +8,12 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.parser.helper;
 import de.uni_stuttgart.vis.vowl.owl2vowl.constants.Vowl_Lang;
 import de.uni_stuttgart.vis.vowl.owl2vowl.parser.container.Annotation;
 import de.uni_stuttgart.vis.vowl.owl2vowl.parser.container.MapData;
-import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLDataPropertyAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLLiteral;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -48,6 +51,23 @@ public class AnnotationParser {
 		}
 
 		return anno;
+	}
+
+	public static Map<String, List<Annotation>> processAndGet(Collection<OWLAnnotation> set, MapData mapData) {
+		Map<String, List<Annotation>> result = new LinkedHashMap<String, List<Annotation>>();
+
+		for (OWLAnnotation owlAnnotation : set) {
+			Annotation current = getAnnotation(owlAnnotation, mapData);
+			String key = current.getIdentifier();
+
+			if (!result.containsKey(key)) {
+				result.put(key, new ArrayList<Annotation>());
+			}
+
+			result.get(key).add(current);
+		}
+
+		return result;
 	}
 
 	// TODO
