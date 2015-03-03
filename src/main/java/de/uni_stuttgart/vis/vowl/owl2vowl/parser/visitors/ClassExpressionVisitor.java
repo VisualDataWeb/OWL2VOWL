afefbf45d96b93dd271d68e1861a9ac82c621e28
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2015-03-03 20:17:48 +0000

    Add visitors.

---
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/visitors/ClassExpressionVisitor.java
@@ -0,0 +1,130 @@
+/*
+ * ClassExpressionVisitor.java
+ *
+ */
+
+package de.uni_stuttgart.vis.vowl.owl2vowl.parser.visitors;
+
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.BaseNode;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.BaseClass;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.SpecialClass;
+import de.uni_stuttgart.vis.vowl.owl2vowl.parser.container.MapData;
+import de.uni_stuttgart.vis.vowl.owl2vowl.parser.container.OntologyInformation;
+import org.semanticweb.owlapi.model.*;
+
+/**
+ *
+ */
+public class ClassExpressionVisitor implements OWLClassExpressionVisitor {
+
+	private MapData information;
+	private OWLEntity entity;
+	private SpecialClass target;
+
+	public ClassExpressionVisitor(MapData information, OWLEntity entity, SpecialClass target) {
+		this.information = information;
+		this.entity = entity;
+		this.target = target;
+	}
+
+	@Override
+	public void visit(OWLClass ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectIntersectionOf ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectUnionOf ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectComplementOf ce) {
+		if (!ce.getOperand().isAnonymous()) {
+			OWLClass owlClass = ce.getOperand().asOWLClass();
+			String iri = owlClass.getIRI().toString();
+			if (!target.getIri().equals(iri)) {
+				BaseNode baseNode = information.getMergedMap().get(iri);
+				baseNode.getExistingComplements().add(target);
+				target.getComplementOf().add(baseNode);
+			}
+		} else {
+			//TODO
+		}
+
+	}
+
+	@Override
+	public void visit(OWLObjectSomeValuesFrom ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectAllValuesFrom ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectHasValue ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectMinCardinality ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectExactCardinality ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectMaxCardinality ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectHasSelf ce) {
+
+	}
+
+	@Override
+	public void visit(OWLObjectOneOf ce) {
+
+	}
+
+	@Override
+	public void visit(OWLDataSomeValuesFrom ce) {
+
+	}
+
+	@Override
+	public void visit(OWLDataAllValuesFrom ce) {
+
+	}
+
+	@Override
+	public void visit(OWLDataHasValue ce) {
+
+	}
+
+	@Override
+	public void visit(OWLDataMinCardinality ce) {
+
+	}
+
+	@Override
+	public void visit(OWLDataExactCardinality ce) {
+
+	}
+
+	@Override
+	public void visit(OWLDataMaxCardinality ce) {
+
+	}
+}
