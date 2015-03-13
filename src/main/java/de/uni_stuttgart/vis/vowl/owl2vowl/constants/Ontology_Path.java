e228c87c9abb7e393f4e02b1e28e499f328437d8
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2015-03-13 11:01:54 +0000

    Update ontologies.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/constants/Ontology_Path.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/constants/Ontology_Path.java
@@ -19,6 +19,7 @@ public class Ontology_Path {
 	public static final String ONTO_CARD = PATH_VARIABLE + "ontovibe/ontovibe_cardinalities.ttl";
 	public static final String LOCAL_TESTING = PATH_VARIABLE + "testing.owl";
 	public static final String STACK = PATH_VARIABLE + "StackExchange.ttl";
+	public static final String TAGONT = PATH_VARIABLE + "tagont.owl";
 
 	// External
 	public static final String EXT_GEONAMES = "http://www.geonames.org/ontology/ontology_v3.1.rdf";
