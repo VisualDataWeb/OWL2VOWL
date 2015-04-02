2f8a511aa082c2ba5883794d36f804db28149fe4
marbaced <marbaced@studi.informatik.uni-stuttgart.de>
2015-04-02 23:09:47 +0000

    Set standard iri for the "type" property.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/constants/Standard_Iris.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/constants/Standard_Iris.java
@@ -4,4 +4,5 @@ public class Standard_Iris {/* STANDARD IRIS */
 	public static final String GENERIC_LITERAL_URI =
 			"http://www.w3.org/2000/01/rdf-schema#Literal";
 	public static final String OWL_THING_CLASS_URI = "http://www.w3.org/2002/07/owl#Thing";
+	public static final String TYPE_URI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 }
