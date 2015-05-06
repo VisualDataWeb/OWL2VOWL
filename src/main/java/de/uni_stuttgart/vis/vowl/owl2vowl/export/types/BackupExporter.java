45c0c99712c743c60b00b7663f67922179c1b9de
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-05-06 18:35:00 +0200

    Add new exporter with method for direct integration in other projects.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/export/types/BackupExporter.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/export/types/BackupExporter.java
@@ -6,11 +6,17 @@
 package de.uni_stuttgart.vis.vowl.owl2vowl.export.types;
 
 /**
- * Simple exporter that saves the
+ * Simple exporter that saves the given text.
  */
 public class BackupExporter implements Exporter {
+	protected String convertedJson;
+
+	public String getConvertedJson() {
+		return convertedJson;
+	}
+
 	@Override
 	public void write(String text) throws Exception {
-		// TODO
+		convertedJson = text;
 	}
 }
