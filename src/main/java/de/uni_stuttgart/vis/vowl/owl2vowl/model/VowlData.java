6279168e4e5ef620a83973180ccbdaa80f477363
Vincent Link <mail@linkvt.de>
2015-09-17 18:57:29 +0200

    Add method for requesting nodes by iri

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/VowlData.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/VowlData.java
@@ -5,20 +5,29 @@
 
 package de.uni_stuttgart.vis.vowl.owl2vowl.model;
 
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.AbstractNode;
 import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.AbstractClass;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.semanticweb.owlapi.model.IRI;
+
+import java.util.HashMap;
 
 /**
  * Contains all data WebVOWL needs.
  */
 public class VowlData {
 
-	private Set<AbstractClass> classes = new HashSet<>();
+	private HashMap<IRI, AbstractClass> classMap = new HashMap<>();
 
 	public void addClass(AbstractClass vowlClass) {
-		classes.add(vowlClass);
+		classMap.put(vowlClass.getIri(), vowlClass);
 	}
 
+	public AbstractNode getNodeForIri(IRI iri) {
+		if (classMap.containsKey(iri)) {
+			return classMap.get(iri);
+		} else {
+			throw new IllegalStateException("Can't find node for passed iri: " + iri);
+		}
+	}
 }
