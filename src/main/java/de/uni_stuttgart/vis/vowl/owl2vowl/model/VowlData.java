47072ddfc472a56cbabe3a17a4514c4fbcca75df
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-09-17 23:44:59 +0200

    Extend vowl data

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/VowlData.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/VowlData.java
@@ -18,7 +18,9 @@ import java.util.Map;
  */
 public class VowlData {
 
-	private HashMap<IRI, AbstractClass> classMap = new HashMap<>();
+	private Map<AbstractEntity, Integer> entityToId = new HashMap<>();
+	private Map<IRI, AbstractEntity> entityMap = new HashMap<>();
+	private Map<IRI, AbstractClass> classMap = new AllEntityMap<>(entityMap);
 
 	public Map<IRI, AbstractClass> getClassMap() {
 		return Collections.unmodifiableMap(classMap);
@@ -34,5 +36,40 @@ public class VowlData {
 		} else {
 			throw new IllegalStateException("Can't find node for passed iri: " + iri);
 		}
+	}
+
+	protected AbstractEntity getEntityForIri(IRI iri) {
+		if (entityMap.containsKey(iri)) {
+			return classMap.get(iri);
+		} else {
+			throw new IllegalStateException("Can't find node for passed iri: " + iri);
+		}
+	}
+
+	public int getIdForIri(IRI iri) {
+		return getIdForEntity(getEntityForIri(iri));
+	}
+
+	public int getIdForEntity(AbstractEntity entity) {
+		if (!entityToId.containsKey(entity)) {
+			entityToId.put(entity, entityToId.keySet().size());
+		}
+
+		return entityToId.get(entity);
+	}
+}
+
+class AllEntityMap<K, V extends AbstractEntity> extends HashMap<K, V> {
+	private HashMap<K, V> mergeMap;
+
+	public <Val extends AbstractEntity> AllEntityMap(Map<K, Val> mergeMap) {
+		this.mergeMap = (HashMap<K, V>) mergeMap;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		super.put(key, value);
+		mergeMap.put(key, value);
+		return value;
 	}
 }
