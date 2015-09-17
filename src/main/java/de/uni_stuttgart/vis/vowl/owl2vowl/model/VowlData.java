97350c82a1d2ccda8416bf01cc66336ff14fd42b
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-09-17 23:00:17 +0200

    Add getter for vowl data

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/VowlData.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/VowlData.java
@@ -7,10 +7,11 @@ package de.uni_stuttgart.vis.vowl.owl2vowl.model;
 
 import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.AbstractNode;
 import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.AbstractClass;
-
 import org.semanticweb.owlapi.model.IRI;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Contains all data WebVOWL needs.
@@ -18,6 +19,10 @@ import java.util.HashMap;
 public class VowlData {
 
 	private HashMap<IRI, AbstractClass> classMap = new HashMap<>();
+
+	public Map<IRI, AbstractClass> getClassMap() {
+		return Collections.unmodifiableMap(classMap);
+	}
 
 	public void addClass(AbstractClass vowlClass) {
 		classMap.put(vowlClass.getIri(), vowlClass);
