79a9fc51e1133092eff916b60226ae4c92f5bb08
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-09-17 23:01:17 +0200

    Create visitor for classes

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/vowl/OwlEquivalentsVisitor.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/vowl/OwlEquivalentsVisitor.java
@@ -6,19 +6,38 @@ import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.VowlClass;
 import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.classes.VowlThing;
 
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
 import org.semanticweb.owlapi.util.OWLObjectVisitorAdapter;
+
+import java.util.Set;
 
 public class OwlEquivalentsVisitor extends OWLObjectVisitorAdapter {
 
 	private VowlData vowlData;
+	private OWLClass owlClass;
 
-	public OwlEquivalentsVisitor(VowlData vowlData) {
+	public OwlEquivalentsVisitor(VowlData vowlData, OWLClass owlClass) {
 		this.vowlData = vowlData;
+		this.owlClass = owlClass;
 	}
 
 	@Override
 	public void visit(OWLEquivalentClassesAxiom axiom) {
-		// TODO implement
+		// TODO NamedClasses size != 1 means either
+		if (axiom.getNamedClasses().size() != 1) {
+			createEquivalentClass(axiom);
+		}
+
+		OWLClass referencedClass = axiom.getNamedClasses().iterator().next();
+
+		Set<OWLClassExpression> expressionsWithoutRefClass = axiom.getClassExpressionsMinus(referencedClass);
+		for (OWLClassExpression anonymExpressions : expressionsWithoutRefClass) {
+			anonymExpressions.accept(new VowlClassVisitor(vowlData, referencedClass));
+		}
+	}
+
+	protected void createEquivalentClass(OWLEquivalentClassesAxiom axiom) {
+
 	}
 }
---
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/parser/vowl/VowlClassVisitor.java
@@ -0,0 +1,47 @@
+/*
+ * VowlClassVisitor.java
+ *
+ */
+
+package de.uni_stuttgart.vis.vowl.owl2vowl.parser.vowl;
+
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.VowlData;
+import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.AbstractNode;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLObjectIntersectionOf;
+import org.semanticweb.owlapi.util.OWLClassExpressionVisitorAdapter;
+
+import java.util.Set;
+
+/**
+ *
+ */
+public class VowlClassVisitor extends OWLClassExpressionVisitorAdapter {
+	private final VowlData vowlData;
+	private final OWLClass referencedClass;
+	Logger logger = LogManager.getLogger(VowlClassVisitor.class);
+
+	public VowlClassVisitor(VowlData vowlData, OWLClass referencedClass) {
+		this.vowlData = vowlData;
+		this.referencedClass = referencedClass;
+	}
+
+	@Override
+	public void visit(OWLObjectIntersectionOf ce) {
+		Set<OWLClassExpression> operands = ce.getOperands();
+
+		AbstractNode node = vowlData.getNodeForIri(referencedClass.getIRI());
+
+		for (OWLClassExpression operand : operands) {
+			if (!operand.isAnonymous()) {
+				node.addElementToIntersection(operand.asOWLClass().getIRI());
+			} else {
+				// TODO Anonymous undefined behaviour
+				logger.info("Anonymous exists in intersections.");
+			}
+		}
+	}
+}
