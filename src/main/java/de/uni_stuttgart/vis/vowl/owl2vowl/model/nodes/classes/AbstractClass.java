e67cd94c0104117a29f29ba4ab430ad21b298ef5
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-10-28 23:44:00 +0100

    Add individuals interface

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/nodes/classes/AbstractClass.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/model/nodes/classes/AbstractClass.java
@@ -9,14 +9,17 @@ import de.uni_stuttgart.vis.vowl.owl2vowl.model.nodes.AbstractNode;
 
 import org.semanticweb.owlapi.model.IRI;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  *
  */
-public abstract class AbstractClass extends AbstractNode implements HasDisjointUnion {
+public abstract class AbstractClass extends AbstractNode implements HasDisjointUnion, HasIndividuals {
 	protected Set<IRI> disjointUnion = new HashSet<>();
+	protected Set<IRI> instances = new HashSet<>();
+	protected Set<IRI> individuals = new HashSet<>();
 
 	protected AbstractClass(IRI iri, String type) {
 		super(iri, type);
@@ -30,5 +33,25 @@ public abstract class AbstractClass extends AbstractNode implements HasDisjointU
 	@Override
 	public void addDisjoint(IRI disjointIri) {
 		disjointUnion.add(disjointIri);
+	}
+
+	@Override
+	public void addIndividual(IRI iri) {
+		individuals.add(iri);
+	}
+
+	@Override
+	public Set<IRI> getIndividuals() {
+		return Collections.unmodifiableSet(individuals);
+	}
+
+	@Override
+	public void addInstance(IRI iri) {
+		instances.add(iri);
+	}
+
+	@Override
+	public Set<IRI> getInstances() {
+		return Collections.unmodifiableSet(instances);
 	}
 }
