71312783694ca2071e37c4beddd6a1c2f4689f5d
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2016-01-21 16:08:55 +0100

    Add output parameter to the CLI. Resolves #8.

--- src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/ConsoleMain.java
+++ src/main/java/de/uni_stuttgart/vis/vowl/owl2vowl/ConsoleMain.java
@@ -22,16 +22,17 @@ import java.util.List;
 public class ConsoleMain {
 	private static final String IRI_OPTION_NAME = "iri";
 	private static final String FILE_OPTION_NAME = "file";
+	private static final String OUTPUT_OPTION_NAME = "output";
 	private static final String DEPENDENCIES_OPTION_NAME = "dependencies";
 	private static final String HELP_OPTION_NAME = "h";
 	private static final String ECHO_OPTION_NAME = "echo";
-	private static final Logger logger = LogManager.getRootLogger();
+	private static final Logger logger = LogManager.getLogger(ConsoleMain.class);
 
 	public static void main(String[] args) {
-		parseCommandLine(args);
+		new ConsoleMain().parseCommandLine(args);
 	}
 
-	private static void parseCommandLine(String[] args) {
+	protected void parseCommandLine(String[] args) {
 		CommandLine line = null;
 		Options options = createOptions();
 
@@ -74,7 +75,7 @@ public class ConsoleMain {
 		}
 	}
 
-	private static Options createOptions() {
+	protected Options createOptions() {
 		Options options = new Options();
 
 		options.addOption(new Option(HELP_OPTION_NAME, "views this help text"));
@@ -97,6 +98,7 @@ public class ConsoleMain {
 
 		OptionGroup outputOptions = new OptionGroup();
 		outputOptions.addOption(new Option(ECHO_OPTION_NAME, "prints the converted ontology on the console"));
+		outputOptions.addOption(Option.builder(OUTPUT_OPTION_NAME).argName("PATH").hasArg().desc("specify the path for the desired output location").build());
 
 		options.addOptionGroup(inputOptions);
 		options.addOptionGroup(outputOptions);
@@ -104,22 +106,35 @@ public class ConsoleMain {
 		return options;
 	}
 
-	private static void printHelpMenuAndExit(Options options) {
+	protected void printHelpMenuAndExit(Options options) {
 		HelpFormatter helpFormatter = new HelpFormatter();
 		helpFormatter.printHelp("java -jar owl2vowl.jar", options);
 		System.exit(0);
 	}
 
-	private static Exporter createExporterFromOption(CommandLine line, IRI ontologyIri) {
+	protected Exporter createExporterFromOption(CommandLine line, IRI ontologyIri) {
 		if (line.hasOption(ECHO_OPTION_NAME)) {
 			return new ConsoleExporter();
 		} else {
-			return generateFileExporter(ontologyIri);
+			String exportPath = null;
+
+			if (line.hasOption(OUTPUT_OPTION_NAME)) {
+				exportPath = line.getOptionValue(OUTPUT_OPTION_NAME);
+			}
+
+			return generateFileExporter(ontologyIri, exportPath);
 		}
 	}
 
-	private static FileExporter generateFileExporter(IRI ontologyIri) {
-		String filename = FilenameUtils.removeExtension(ontologyIri.getRemainder().get()) + ".json";
+	protected FileExporter generateFileExporter(IRI ontologyIri, String filePath) {
+		String filename;
+
+		if (filePath != null) {
+			filename = filePath;
+		} else {
+			filename = FilenameUtils.removeExtension(ontologyIri.getRemainder().get()) + ".json";
+		}
+
 		return new FileExporter(new File(filename));
 	}
 }
---
+++ src/test/java/de/uni_stuttgart/vis/vowl/owl2vowl/ConsoleMainTest.java
@@ -0,0 +1,55 @@
+package de.uni_stuttgart.vis.vowl.owl2vowl;
+
+import de.uni_stuttgart.vis.vowl.owl2vowl.export.types.FileExporter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.semanticweb.owlapi.model.IRI;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyCollection;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ConverterImpl.class)
+@PowerMockIgnore( {"javax.management.*"})
+public class ConsoleMainTest extends ConsoleMain {
+
+	private static final String TEST_PATH = "testPfad";
+	private static final String TEST_IRI = "http://testiri.de";
+
+	@Test
+	public void testMain() throws Exception {
+		ConverterImpl mockedConverter = mock(ConverterImpl.class);
+		doNothing().when(mockedConverter).convert();
+
+		PowerMockito.whenNew(ConverterImpl.class).withArguments(Mockito.any(IRI.class), anyCollection()).thenReturn(mockedConverter);
+
+		ConsoleMain.main(new String[]{"-iri", "http://test.de"});
+	}
+
+
+	@Test
+	public void testOutputParameter() throws Exception {
+		ConverterImpl mockedConverter = mock(ConverterImpl.class);
+		doNothing().when(mockedConverter).convert();
+		PowerMockito.whenNew(ConverterImpl.class).withArguments(Mockito.any(IRI.class), anyCollection()).thenReturn(mockedConverter);
+
+		FileExporter mockedFileExporter = mock(FileExporter.class);
+		PowerMockito.whenNew(FileExporter.class).withArguments(Mockito.any(File.class)).thenAnswer(invocationOnMock -> {
+			File file = (File) invocationOnMock.getArguments()[0];
+			assertThat(file.getName(), is(TEST_PATH));
+			return mockedFileExporter;
+		});
+
+		new ConsoleMain().parseCommandLine(new String[]{"-output", TEST_PATH, "-iri", TEST_IRI});
+	}
+}
